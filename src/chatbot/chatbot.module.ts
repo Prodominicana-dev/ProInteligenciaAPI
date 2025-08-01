33a36d402f894e9b3229f8c35ac7efe466e784b8
Randy Frias <69165606+randy140801@users.noreply.github.com>
2025-08-01 13:46:37 -0400

    Chatbotv2 (#5)
    * chatbot
    
    * IED and Export endpoints for chatbot

--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -1,25 +1,45 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { ChatbotService } from './chatbot.service';
 import { ChatbotController } from './chatbot.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'mssql',
-        url: process.env.DATAMARKET_DATABASE_URL,
-        entities: [],
-        synchronize: false,
-        options: {
-          encrypt: false,
-          trustServerCertificate: true,
-        },
-      }),
-    }),
+  providers: [
+    {
+      provide: 'DataSource_analytica',
+      useFactory: async () => {
+        const dataSource = new DataSource({
+          type: 'mssql',
+          url: process.env.DATAMARKET_ANALYTICA_DATABASE_URL,
+          entities: [],
+          synchronize: false,
+          options: {
+            encrypt: false,
+            trustServerCertificate: true,
+          },
+        });
+        return dataSource.initialize();
+      },
+    },
+    {
+      provide: 'DataSource_ceird',
+      useFactory: async () => {
+        const dataSource = new DataSource({
+          type: 'mssql',
+          url: process.env.DATAMARKET_CEIRD_DATABASE_URL,
+          entities: [],
+          synchronize: false,
+          options: {
+            encrypt: false,
+            trustServerCertificate: true,
+          },
+        });
+        return dataSource.initialize();
+      },
+    },
+    ChatbotService,
   ],
   controllers: [ChatbotController],
-  providers: [ChatbotService],
   exports: [ChatbotService],
 })
 export class ChatbotModule {}
